fd41c92f1a8069655d63b23f8b35fb59dc279511
jpsk <pask.mindaugas@gmail.com>
2017-12-07 14:28:23 +0200

    Day 1 init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  disabled = true;
+  input = new FormControl();
+  sub = new Subscription();
+  output;
+
+  ngOnInit() {
+    this.sub.add(this.input.valueChanges.subscribe(i => {
+      if (!i) { this.output = undefined; }
+      this.disabled = !i
+    }));
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onSubmit() {
+    this.output = this.input.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatToolbarModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +19,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
